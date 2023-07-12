99a1a76b898fa0d8fc0a9843d87bae8c9202b5bf
Charles Crane <charlie.crane46@gmail.com>
2023-07-12 15:26:59 +0100

    add fathom

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Fathom from './(components)/Fathom'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Fathom />
+      </body>
     </html>
   )
 }
